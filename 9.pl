26267ee859c7ff27fa0f07de43ecf251192fa957
avtss <avetiszadikyann@mail.ru>
2025-04-23 14:06:25 +0300

    9.1

--- 9.pl
+++ 9.pl
@@ -25,3 +25,34 @@ my_ap([X|Tail], Y, [X|Tail]) :- my_ap(Tail, Y, Tail1).
 in_list([], _) :- false.
 in_list([X|Tail], X).
 in_list([_|T], X) :- in_list(T, X).
+
+fact_up(0, 1) :- !.
+fact_up(N, F) :-
+    N > 0,
+    N1 is N - 1,
+    fact_up(N1, F1),
+    F is N * F1.
+
+fact_down(N, F) :- fact_down(N, 1, F).
+fact_down(0, Acc, Acc) :- !.
+fact_down(N, Acc, F) :-
+    N > 0,
+    NewAcc is Acc * N,
+    N1 is N - 1,
+    fact_down(N1, NewAcc, F).
+
+is_square_free(N) :-
+    N > 0,
+    \+ (between(2, N, D),
+        D*D =< N,
+        0 is N mod (D*D)).
+
+r_list(A, N):-r_list(A, N, 0, []).
+r_list(A, N, N, A):-!.
+r_list(A, N, K, B):-read(X), append(B,[X],B1), K1 is K+1, r_list(A, N, K1, B1).
+
+w_list([]):-!.
+w_list([H|T]) :- write(H), nl, w_list(T).
+
+my_ap([], Y, Y).
+my_ap([H|T], Y, [H|T1]) :- my_ap(T, Y, T1).
