a20f11d1f9877f6983256b854e9532d8a39e3aac
avtss <avetiszadikyann@mail.ru>
2025-05-24 00:12:02 +0300

    Update IZ.pl

--- IZ.pl
+++ IZ.pl
@@ -86,11 +86,3 @@ find_base(Vertices, Base) :-
 subset([], []).
 subset([X|Xs], [X|Ys]) :- subset(Xs, Ys).
 subset(Xs, [_|Ys]) :- subset(Xs, Ys).
-
-% === Helper Predicates ===
-read_str(A) :- get0(X), r_str(X, A, []).
-
-r_str(10, A, A) :- !.
-r_str(X, A, B) :- append(B, [X], B1), get0(X1), r_str(X1, A, B1).
-
-del_1st([H|T], T).
